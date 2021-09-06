a6d3e1c8370a94802483a6d275b793cb026de12c
Mehdi <elwafi.courrier99@gmail.com>
2021-09-06 23:12:17 +0100

    Added  my first test case add 2 ints

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -1,5 +1,5 @@
 public class MathUtils {
     public int add(int a,int b) {
-        return a * b;
+        return a + b;
     }
 }
--- src/test/java/MyFirstJUnitTest.java
+++ src/test/java/MyFirstJUnitTest.java
@@ -1,8 +1,12 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MyFirstJUnitTest {
     @Test
     public void test() {
-        System.out.println("Running my tests");
+        MathUtils mathUtils = new MathUtils();
+        int expect = 7;
+        int result = mathUtils.add(4,8);
+        Assertions.assertEquals(expect,result,"Should Add two numbers");
     }
 }
