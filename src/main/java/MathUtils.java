ddf37ea94adbbfbb13dcc8c9eff798aac3fbf6f3
Mehdi <elwafi.courrier99@gmail.com>
2021-09-26 15:46:24 +0100

    Added multiply method to math utils

--- src/main/java/MathUtils.java
+++ src/main/java/MathUtils.java
@@ -2,6 +2,7 @@ public class MathUtils {
     public int add(int a,int b) {
         return a + b;
     }
+    public int multiply(int x,int y){return x * y;}
     public Double calculateAreaDisc(Double x){
         return Math.PI * x * x;
     }
